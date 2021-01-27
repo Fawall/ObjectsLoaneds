9ed9f1605f9a3143110336fa8e40f9bf1a0b5f51
fabio lopes <fabiolopes7844@gmail.com>
2021-01-26 22:55:46 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ObjectsLoaneds.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
