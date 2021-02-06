cce235f337ba25627b59d1a0f845c3ee3d17e12a
fabio lopes <fabiolopes7844@gmail.com>
2021-02-06 01:18:07 -0300

    Rename class Context for ApplicationDbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ObjectsLoaneds.Models;
+
+
+namespace ObjectsLoaneds.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<ObjectsLoanedModel> ObjectsLoaneds { get; set; }
+        
+
+    }
+}
